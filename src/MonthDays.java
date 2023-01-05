94252043d14b900a4a89a9742f22fb226c5fa9b1
tamara Vanyan <tam.vanyan@gmail.com>
2023-01-06 01:11:46 +0400

    Add tasks  updating files

--- src/MonthDays.java
+++ src/MonthDays.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 public class MonthDays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the year: ");
+        int year = scanner.nextInt();
 
         System.out.print("Enter the month number in range 1 to 12: ");
         int month = scanner.nextInt();
-
-        int days;
-        switch (month) {
+        int days = 0;
+       /*arajin tarberak kaij, aranc tarin hashvi arnelu ev ankap :DD
+       switch (month) {
             case 2:
                 days = 28;
                 break;
@@ -28,8 +30,59 @@ public class MonthDays {
                 break;
             default:
                 System.out.println("Invalid month number");
-                System.exit(0);
+                System.exit(1);
                 return;
+        }
+        System.out.println("Month " + month + " has " + days + " days");
+*/
+        //erkrord tarberak__________________________
+        if (1 <= month && month <= 12) {
+            if (year % 4 != 0) {
+                switch (month) {
+                    case 2:
+                        days = 28;
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        days = 30;
+                        break;
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        days = 31;
+                        break;
+                }
+            } else {
+                switch (month) {
+                    case 2:
+                        days = 29;
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        days = 30;
+                        break;
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        days = 31;
+                        break;
+                }
+            }
+        } else {
+            System.out.println("Invalid month number");
+            System.exit(1);
         }
 
         System.out.println("Month " + month + " has " + days + " days");
--- src/Triangle.java
+++ src/Triangle.java
@@ -4,10 +4,17 @@ public class Triangle {
         Scanner myObj = new Scanner(System.in);
 
         System.out.println("Enter the sides of the triangle");
+        System.out.println("Enter a: ");
         int a = myObj.nextInt();
+        System.out.println("Enter b: ");
         int b = myObj.nextInt();
+        System.out.println("Enter c: ");
         int c = myObj.nextInt();
-
+        //erkarutyuny chi karox linel bacasakan tiv
+        if(a < 0 || b < 0 || c < 0){
+            System.out.println("The length of the side of a triangle cannot be a negative number");
+            System.exit(1);
+        }
         // erankyan 2 koxmeri gumary misht mec e errord koxmic
         if ((a + b > c) && (a + c > b) && (b + c > a)) {
             System.out.println("Triangle is possible");
