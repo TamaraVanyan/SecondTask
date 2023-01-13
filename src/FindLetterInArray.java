3e239b2b863b3012b443ef5815d623a7423fd18d
tamara Vanyan <tam.vanyan@gmail.com>
2023-01-13 10:21:08 +0400

    Add some changes in finding letter in array task

--- src/FindLetterInArray.java
+++ src/FindLetterInArray.java
@@ -5,16 +5,16 @@ public class FindLetterInArray {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a letter: ");
         char letter = scanner.next().charAt(0);
-        int index = 0;
+        int index = -1;
         int i = 0;
-        while (i < array.length && index == 0) {
+        while (i < array.length) {
             if (array[i] == letter) {
                 index = i;
                 break;
             }
             i++;
         }
-        if (index != 0) {
+        if (index != -1) {
             System.out.println("Letter found at index: " + index);
         } else {
             System.out.println("Letter not found in the array");
