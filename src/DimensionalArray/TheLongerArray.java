c285f6b1a7ed736e30177759c613cb3963df69e0
tamara Vanyan <tam.vanyan@gmail.com>
2023-01-15 20:30:38 +0400

    Add some changes in longer array task

--- src/DimensionalArray/TheLongerArray.java
+++ src/DimensionalArray/TheLongerArray.java
@@ -2,14 +2,15 @@ package DimensionalArray;
 public class TheLongerArray {
     public static void main(String[] args) {
          int[][] mixedArray = {{5, 15, 25, 35, 65},  {3, 8}, {-6, 66, 666}, {0, 8, 18}, {45, 88, 77, 99, 102, 1}, {152148}};
-         int longerArray = mixedArray[0].length;
-        System.out.print("The largest array in main array is: ");
+         int[] longerArray = mixedArray[0];
          for (int i = 0; i < mixedArray.length; i++) {
-            for (int j = 0; j < mixedArray[i].length; j++) {
-                if(mixedArray[i].length > longerArray){
-                    System.out.print(mixedArray[i][j] + " ");
+                if(mixedArray[i].length > longerArray.length){
+                    longerArray = mixedArray[i];
                 }
-            }
+        }
+        System.out.print("The largest array in main array is: ");
+        for (int j = 0; j < longerArray.length;  j++) {
+            System.out.print(longerArray[j] + " ");
         }
     }
 }
