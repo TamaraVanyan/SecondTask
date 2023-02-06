c7a29da2c449038a0d34f802657b4d1106a2a545
tamara Vanyan <tam.vanyan@gmail.com>
2023-02-06 23:40:19 +0400

    Some changes in task about repieted word in text

--- src/StringMethod/RepeatedWords.java
+++ src/StringMethod/RepeatedWords.java
@@ -2,32 +2,27 @@ package StringMethod;
 
 public class RepeatedWords {
     public static void main(String[] args) {
-        System.out.println(wordCount("In this text this repeated two time", "this"));
+        System.out.println(wordCount("In this text this repeated two time this this", "this"));
         System.out.println(wordCount("some text", "bla"));
         System.out.println(wordCount("some text", ""));
-        System.out.println(stringWordCount("string word repeated three time in this string type sentance like string"));
-        System.out.println(stringWordCount(""));
-        System.out.println(stringWordCount("some random text"));
+        System.out.println(wordCount("string word repeated three time in this string type sentance like string"));
+        System.out.println(wordCount(""));
+        System.out.println(wordCount("some random text"));
     }
+
     public static int wordCount(String text, String word) {
         if (text.isEmpty() || word.isEmpty()) {
             return -2;
         }
-        int count = text.split(word).length - 1;
-        if (count == 0) {
+        int count = text.split(word).length;
+        if (count == 1) {
             return -1;
+        } else {
+            return count;
         }
-        return count;
     }
-    public static int stringWordCount(String text) {
-        if (text.isEmpty()) {
-            return -2;
-        }
 
-        int count = text.split("string").length - 1;
-        if (count == 0) {
-            return -1;
-        }
-        return count;
+    public static int wordCount(String text) {
+        return wordCount(text, "string");
     }
 }
