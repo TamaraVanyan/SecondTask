4e6982a9cbdb818b00592e4796f85fb6cf1259f3
tamara Vanyan <tam.vanyan@gmail.com>
2023-02-24 01:22:37 +0400

    Add task about sorted array in ascending order

--- src/sortedArray/SortedNumbersInArray.java
+++ src/sortedArray/SortedNumbersInArray.java
@@ -3,33 +3,23 @@ package sortedArray;
 public class SortedNumbersInArray {
     public static void main(String[] args) {
         int[] arr = {5, -23, 5, 12, -23, 5, 5, 5, -23, 12, -23, 12, 5, -23};
-        int n = arr.length;
-        int n23 = 0;
-        int n5 = 0;
-        int n12 = 0;
+        int n;
 
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == -23) {
-                n23++;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    n = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = n;
+                }
             }
-            if (arr[i] == 5) {
-                n5++;
-            }
-            if (arr[i] == 12) {
-                n12++;
-            }
-
         }
+
+        System.out.print("Sorted array in ascending Order: ");
         System.out.print("{");
-        for (int i = 0; i < n23; i++) {
-            System.out.print(-23 + ", ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
         }
-        for (int i = 0; i < n5; i++) {
-            System.out.print(5 + ", ");
-        }
-        for (int i = 0; i < n12; i++) {
-            System.out.print(12 + ", ");
-        }
-        System.out.println("}");
+        System.out.print("}");
     }
 }
